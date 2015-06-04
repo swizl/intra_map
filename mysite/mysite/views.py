e6911d82d538c59d36b49e512be6bba4233439b1
小李摧花 <i@xlch.cn>
2015-06-04 16:27:43 +0800

    add more

---
+++ mysite/mysite/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required  
+ 
+@login_required(login_url="/login/")  
+def home(request):
+	return HttpResponse('Welcome, <a target="_blank" href="/logout/">logout</a>')
